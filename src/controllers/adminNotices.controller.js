795249a818611425499ab62276b0c436b0639fed
marshmello <pesbg123@gmail.com>
2023-08-22 11:33:20 +0900

    chore: adminNotice 3계층 기초 틀 완성

---
+++ src/controllers/adminNotices.controller.js
@@ -0,0 +1,17 @@
+const AdminNoticesService = require('../services/adminNotices.service');
+
+class AdminNoticesController {
+  constructor() {
+    this.adminNoticesService = new AdminNoticesService();
+  }
+
+  // admin 공지 생성
+
+  // admin 공지 조회
+
+  // admin 공지 삭제
+
+  // admin 공지 수정
+}
+
+module.exports = AdminNoticesController;
---
+++ src/services/adminNotices.service.js
@@ -0,0 +1,17 @@
+const AdminNoticesRepository = require('../repositories/adminNotices.repository');
+
+class AdminNoticesService {
+  constructor() {
+    this.aminNoticesRepository = new AdminNoticesRepository();
+  }
+
+  // admin 공지 생성
+
+  // admin 공지 조회
+
+  // admin 공지 삭제
+
+  // admin 공지 수정
+}
+
+module.exports = AdminNoticesService;
