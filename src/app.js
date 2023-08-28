76b2a089edc6c24946af4b734fb35056783d6246
rakulow2 <133615911+rakulow2@users.noreply.github.com>
2023-08-28 20:41:33 +0900

    fix:comment라우터 추가

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ const path = require('path');
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
+const path = require('path');
 
+const commentsRouter = require('./routes/comments.routes');
 const adminNoticeRouter = require('./routes/admin.notices.routes');
 const postLikeRouter = require('./routes/postLike.routes');
 const userFollowRouter = require('./routes/userFollow.routes');
@@ -13,7 +15,7 @@ const postRouter = require('./routes/posts.routes');
 
 app.use(express.json());
 
-app.use('/api', [adminNoticeRouter, hashTagRouter, postLikeRouter, userFollowRouter, hashTagRouter, adminUserBanRouter, postRouter]);
+app.use('/api', [adminNoticeRouter, hashTagRouter, postLikeRouter, userFollowRouter, hashTagRouter, adminUserBanRouter, postRouter,commentsRouter]);
 
 app.use(express.static(path.join(__dirname, './public')));
 
