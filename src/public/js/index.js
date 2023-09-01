af3caba89e02d6d5d07d904926b8d8f5c63101be
parkchaewon <xkdldjaos20@gmail.com>
2023-09-01 20:23:26 +0900

    무한 스크롤 +메인 화면CSS

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,35 +1,38 @@
 $(document).ready(() => {
   getPosts();
 });
+
 const getPosts = async () => {
   try {
     const response = await axios.get('/api/posts');
-    console.log(response);
+    const posts = response.data.data;
+
     let allHtml = '';
-    response.data.data.forEach((item) => {
-      let tempHtml = `<div class="col"> <div class="card shadow-sm">
-      <svg class="bd-placeholder-img card-img-top" width="20000%" height="225" 
-            
-              <img src="${item.food_img}" alt="${item.title}" class="card-img-top">
-              <div class="card-body">
-                <p class="card-text">${item.title}</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">ㅎ</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">ㅇ</button>
-                  </div>
-                  <small class="text-body-secondary">9분</small>
+    posts.forEach((item) => {
+      let tempHtml = `
+        <div class="col-md-4 mb-4">
+          <div class="card shadow-sm">
+            <img src="${item.food_img}" alt="${item.title}" class="card-img-top">
+            <div class="card-body">
+              <p class="card-text">${item.title}</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">기본</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">기본</button>
                 </div>
+                <small class="text-body-secondary">${item.createdAt}</small>
               </div>
             </div>
-          </div>`;
+          </div>
+        </div>`;
       allHtml += tempHtml;
     });
 
-    $('#card-list').html(allHtml);
+    $('#card-list').append(allHtml);
 
-    const lastLi = document.querySelector('.feed-container > li:last-child');
-    io.observe(lastLi);
+    pageCount++;
+    const lastCard = document.querySelector('#card-list > .col-md-4:last-child');
+    io.observe(lastCard);
   } catch (error) {
     console.log(error);
   }
@@ -42,27 +45,33 @@ const io = new IntersectionObserver(async (entries, observer) => {
       pageCount++;
       try {
         const response = await axios.get(`/api/posts?page=${pageCount}`);
-        const rows = response.data;
+        const rows = response.data.data;
 
-        const feedList = document.getElementById('feedList');
+        const cardList = document.getElementById('card-list');
 
         rows.forEach((row) => {
-          const feedItem = document.createElement('li');
-          feedItem.classList.add('feed-item');
-          feedItem.innerHTML = `
-            <a href="/detail/${row.post}">
-              <div class="img-container">
-                <img src="${row.imageUrl}" style="border-radius: 10px;" />
+          const card = document.createElement('div');
+          card.classList.add('col-md-4', 'mb-4');
+          card.innerHTML = `
+            <div class="card shadow-sm">
+              <img src="${row.food_img}" alt="${row.title}" class="card-img-top">
+              <div class="card-body">
+                <p class="card-text">${row.title}</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">무한</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">무한</button>
+                  </div>
+                  <small class="text-body-secondary">${row.createdAt}</small>
+                </div>
               </div>
-              <h3>${row.name}</h3>
-              <p style="color:gray;">${row.foodType}</p>
-            </a>
+            </div>
           `;
-          feedList.appendChild(feedItem);
+          cardList.appendChild(card);
         });
 
-        const lastLi = feedList.querySelector('.feed-item:last-child');
-        io.observe(lastLi);
+        const lastCard = cardList.querySelector('.col-md-4:last-child');
+        io.observe(lastCard);
       } catch (error) {
         console.error('데이터 가져오기 오류:', error);
       }
@@ -72,4 +81,3 @@ const io = new IntersectionObserver(async (entries, observer) => {
 
 const form = document.querySelector('#postForm');
 form.addEventListener('submit', post);
-
